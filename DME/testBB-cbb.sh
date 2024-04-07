ea1132d03c6818b27a70f0711b5ac74d8895c9fe
BingLuCS <lu2020cjc@gmail.com>
2024-04-07 21:53:45 +0800

    update

---
+++ DME/testBB-cbb.sh
@@ -0,0 +1,2 @@
+echo -en '\tPFS = "'"$CBB_HOME"'/run/PFS/nocompress/",\n\tBB0 = "'"$CBB_HOME"'/run/BB/cbb/",\n\tBB1 = "'"$CBB_HOME"'/run/BB/sim_bb/",
+\tjson_file = "'"$CBB_HOME"'/DME/file_cache_cbb.json",\n\tthreshold = 1024*1024*1024*50.0' > BBconfig
