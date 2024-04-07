064c823fe112ef02d3b3aeb5cb5399e8c4eb99ec
BingLuCS <lu2020cjc@gmail.com>
2024-04-07 23:08:16 +0800

    update

--- sim_bb/sim_file.py
+++ sim_bb/sim_file.py
@@ -2,44 +2,40 @@ import os
 import sys 
 import re
 import subprocess
-import json
-import tqdm
-prefix = '/home/ubutnu/hardDisk/DeepLearning/wiki/'
+prefix = '/home/ubutnu/hardDisk/val/CBB/run/PFS/nocompress/'
 def run_test_for_files(directory, script =None):
-    # 获取目录下所有文件
+
     filename_list = []
-    filepath_list = []
+    path_list = []
+    subpath_list = []
     dpzip_info = {} 
     for root, directories, files in os.walk(directory):
         for filename in files:
             tmp = os.path.join(root, filename)
-            filepath_list.append(tmp)
-            #tmp = tmp[len(prefix):]
+            path_list.append(tmp)
+            tmp = tmp[len(prefix):]
+            subpath_list.append(tmp)
             filename_list.append(filename)
-        #print(directories)
-    # 遍历文件
-    for i,file in  enumerate(filepath_list):
-        # if i==2: 
-        #     break
+
+
+    for i,file in  enumerate(path_list):
+        if i==2: 
+            break
         if os.path.isfile(file):
-            # 构建运行命令
             command = f"python3 {script} {file}"
         result = subprocess.run(command,shell=True, capture_output=True, text=True).stdout.strip().replace('\n', ' ')
         match = re.search(r'Total compressed bytes: (\d+)', result)
         size = float(match.group(1))
-        dpzip_info[filename_list[i]] = {"size": size}
+        # dpzip_info[filename_list[i]] = {"size": size}
+        # print(filename_list[i],size)
+        command = f"dd if=/dev/zero of={filename_list[i]} bs={size} count=1"
+        os.system(command)
     # with open('2dpzip_info.json', 'w') as f:
     #     json.dump(dpzip_info, f, indent=4)
-    #     command = f"dd if=/dev/zero of={filename_list[i]} bs={size} count=1"
-    #     os.system(command)
+
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 3:
-    #     print("Usage: python3 run_test_for_files.py directory script")
-    #     sys.exit(1)
-    
-    # directory = sys.argv[1]  # 目录路径
-    # script = sys.argv[2]  # 脚本路径
-    directory = '/home/ubutnu/hardDisk/DeepLearning/wiki_book2/'
-    run_test_for_files(directory,"/home/ubutnu/hardDisk/test/zip_sim.py dpzip" )
+    directory = sys.argv[1]
+    print(directory[:-4])
+    run_test_for_files(directory,"./zip_sim.py dpzip" )
 
