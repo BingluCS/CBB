da5f2aee8568a00e1f72da5589eea593791f543e
Bing LU <lu2020cjc@gmail.com>
2024-04-10 19:27:10 +0800

    First version

--- warpx.sh
+++ warpx.sh
@@ -4,12 +4,10 @@ git clone https://github.com/ECP-WarpX/warpx-data.git
 
 cd WarpX
 make clean
-cp $CBB_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.ori $CBB_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
 make -j 8 USE_HDF5_SZ3=FALSE 
 mv main3d* nocomp
 
 make clean
-cp $CBB_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.ori $CBB_HOME/amrex/Src/Extern/HDF5/AMReX_PlotFileUtilHDF5.cpp
 make -j 8 USE_HDF5_SZ3=TRUE SZ3_HOME=$SZ3_HOME
 mv main3d* comp
 
